8e90508e69297af6f475efa347ac663a8d1ecc85
JagadeeswaranG <110896732+JagadeeswaranG@users.noreply.github.com>
2022-09-01 14:57:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// let ele=document.createElement("div");
+// ele.setAttribute("class","container");
+// let ele1=document.createElement("div");
+// ele1.setAttribute("class","row");
+// let ele2=document.createElement("div");
+// ele2.setAttribute("class","col-1");
+
+// ele.append(ele1);
+// ele1.append(ele2)
+// document.body.append(ele);
+
+function foo(){
+    var fn=document.getElementById("fn").value;
+    console.log(fn);
+
+
+    var mn=document.getElementById("mn").value;
+    console.log(mn);
+
+
+    var ln=document.getElementById("ln").value;
+    console.log(ln);
+
+
+    var en=document.getElementById("em").value;
+    console.log(en);
+}
